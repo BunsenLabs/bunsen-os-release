6a1e074c59e232c3c71b34a157176532650e497b
Jens John <dev@2ion.de>
2019-01-31 11:40:04 +0100

    changelog, make: bump version to 10.0 (Lithium)

--- config.mk
+++ config.mk
@@ -4,8 +4,8 @@ include /usr/include/gmsl
 # OS identifiers
 #
 
-OS_CODENAME       = Helium
-OS_VERSION_ID     = 9.7
+OS_CODENAME       = Lithium
+OS_VERSION_ID     = 10.0
 OS_VERSION        = $(OS_CODENAME) ($(OS_VERSION_ID))
 OS_COLOR          = 0;34
 OS_BUG_REPORT_URL = https://forums.bunsenlabs.org/viewforum.php?id=14
