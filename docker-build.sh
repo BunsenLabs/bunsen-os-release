5461352bba8c07863d54d502ee6ac2a41ce365b4
Jens John <dev@2ion.de>
2019-08-25 11:15:05 +0200

    debian: Fix issues with obsolete config files not getting removed

--- docker-build.sh
+++ docker-build.sh
@@ -2,15 +2,25 @@
 
 sudo rm -rf -- "$PWD"/build
 sudo docker run -v "$PWD:/mnt" debian:buster /bin/bash -c '
+  set -e;
+
   echo "Installing dependencies...";
-  apt-get update &>/dev/null;
-  apt-get install -yy debhelper config-package-dev  pandoc make gmsl rsync &>/dev/null;
+
+  echo -ne "\e[0;34m"
+  apt-get update;
+  apt-get install -yy debhelper config-package-dev pandoc make gmsl rsync;
+  echo -ne "\e[0m";
+
   echo "Copying source...";
   mkdir -p /tmp/build;
   rsync --archive /mnt/ /tmp/build/;
+
   echo "Building...";
+  echo -ne "\e[0;34m"
   cd /tmp/build/;
   dpkg-buildpackage -b -rfakeroot -us -uc;
+  echo -ne "\e[0m";
+
   echo "Copying results...";
   mkdir -p /mnt/build;
   rsync /tmp/*.deb /mnt/build/;
